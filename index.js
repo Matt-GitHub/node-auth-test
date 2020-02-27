616a088ef0721db8045e0e361ff2a162cb8a92e0
Matt-GitHub <matthewjamesbedard@outlook.com>
2020-02-26 22:42:19 -0500

    ahfals

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const helmet = require("helmet");
+const cors = require("cors");
 const port = process.env.PORT || 6400;
 const userRouter = require("./db/api/index");
 const server = express();
 
 server.use(express.json());
 server.use(helmet());
+server.use(cors());
 server.use("/api", userRouter);
 
 server.listen(port, () => {
